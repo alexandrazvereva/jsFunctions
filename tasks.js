0a06a06c2a162fef9ccdfd406547e268fbb450b5
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-04-14 09:53:53 -0600

    feat:add exercise solution

--- tasks.js
+++ tasks.js
@@ -2,7 +2,7 @@
 //например: "Hello, Igor!"
 
 function sayHello(userName) {
-    //ваш код тут
+   console.log (`Hello ` + userName)
 }
 
 sayHello("Irina");
@@ -13,16 +13,27 @@ sayHello("Constancia");
 
 //Задача 2. Дополните функцию, чтобы она выводила в консоль отсортированный массив, 
 //в котором будут только те цифры, которые кратны 10.
+let newArr;
+function sortNumbers(arr) { 
+    for (let i = 0; i < arr.length; i++) { newArr = arr.filter(i => i % 10 === 0) }
+return newArr;
 
-function sortNumbers() {
-    //ваш код 
 }
 
-sortNumbers([10, 18, 115, 22, 30, 2000, 7]); //[10, 30, 2000]
-sortNumbers([100, 3, 286, 11, 20, 2001, 14]); //[100, 20]
-sortNumbers([17, 95, 410, 4832, 2, 27]); //[410]
+console.log(sortNumbers([10, 18, 115, 22, 30, 2000, 7])); //[10, 30, 2000]
+console.log(sortNumbers([100, 3, 286, 11, 20, 2001, 14])); //[100, 20]
+console.log(sortNumbers([17, 95, 410, 4832, 2, 27])); //[410]
 
 
 //Задача 3. Напишите функцию, которая принимает 1 параметр - слово, и выводит в консоль
 // его длину. Напишите несколько выводов этой функции с разными параметрами, чтобы убедиться,
 //что все работает
+
+function showLength (word) {
+    console.log (word.length)
+}
+showLength("apple");
+showLength("project");
+showLength("assistance");
+showLength("sequence");
+showLength("screenwriter")
